b4d465c5ae7dd9f03ade09c707b9364932f0f011
Stephen Taylor Witte <stevetwitte@gmail.com>
2017-02-19 11:50:27 -0500

    add bootstrap template and step command to use it

---
+++ lib/trimtool/cli/rails/bootstrap_template.rb
@@ -0,0 +1,42 @@
+gem 'rails'
+gem 'pg'
+gem 'puma'
+gem 'bcrypt'
+gem 'rack-cors'
+gem 'active_model_serializers'
+gem 'jwt'
+gem 'knock'
+gem 'cancancan'
+gem 'rolify'
+gem 'seedbank'
+gem 'figaro'
+
+gem_group :development, :test do
+  gem 'byebug', platform: :mri
+  gem 'rspec-rails'
+  gem 'faker'
+  gem 'pry'
+  gem 'pry-byebug'
+  gem 'pry-clipboard'
+  gem 'pry-doc'
+  gem 'pry-docmore'
+  gem 'pry-rescue'
+  gem 'pry-stack_explorer'
+  gem 'pry-rails'
+  gem 'shoulda-matchers'
+end
+
+gem_group :development do
+  gem 'listen'
+  gem 'spring'
+  gem 'spring-watcher-listen'
+end
+
+gem_group :test do
+  gem 'simplecov', :require => false
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
--- lib/trimtool/cli/rails/main.rb
+++ lib/trimtool/cli/rails/main.rb
@@ -9,7 +9,7 @@ module Trimtool
           File.dirname(__FILE__)
         end
 
-        desc 'new APPNAME', 'Create a new base Rails App inside a Docker Container with Postgres setup as the database.'
+        desc 'rails new AppName', 'Create a new base Rails App inside a Docker Container with Postgres setup as the database.'
         method_option :verbose, type: :boolean, default: false, :aliases => '-v', :desc => 'default: [--no-verbose] By default rails new will be run with the quiet flag, this turns it off.'
         method_option :api, type: :boolean, default: false, :desc => 'default: [--no-api] Generate Rails App in API mode.'
         method_option :bootstrap, type: :boolean, default: false, :desc => 'default: [--no-bootstrap] Generate a base Rails app with custom Gemfile and configs. (This along with "--api" is the base for new Rails APIs at Trim Agency).'
@@ -25,17 +25,18 @@ module Trimtool
 
           rails_new_command = 'docker-compose run app rails new . --database=postgresql --skip-bundle'
 
-          # Use our base gemfile don't let rails generate a new one
+          # Use the bootstrap application template
           if options[:bootstrap] && options[:api]
-            copy_file 'BootstrapGemfile', "#{snake_name}/Gemfile"
-            rails_new_command.concat(' --skip')
+            copy_file 'bootstrap_template.rb', "#{snake_name}/bootstrap_template.rb"
+            rails_new_command.concat(' -m bootstrap_template.rb')
           elsif options[:bootstrap]
-            puts '--bootstrap must be used with --api... Bootstrap base rails apps soon.'
+            puts '--bootstrap must be used with --api... Bootstrap base Rails apps soon.'
             return
-          else
-            copy_file 'BaseGemfile', "#{snake_name}/Gemfile"
-            rails_new_command.concat(' --force')
           end
+
+          # Copy this over so that Docker can run Rails new
+          copy_file 'BaseGemfile', "#{snake_name}/Gemfile"
+          rails_new_command.concat(' --force')
 
           if options[:api]
             rails_new_command.concat(' --api')
