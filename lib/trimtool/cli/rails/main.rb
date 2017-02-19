6182090002c621beb743ba5df2b7ce8cc59e3075
Stephen Taylor Witte <stevetwitte@gmail.com>
2017-02-19 13:21:15 -0500

    organize code move templates to own namespace update templates to contain more of the rails config code clean up rails::main

--- lib/trimtool/cli/rails/main.rb
+++ lib/trimtool/cli/rails/main.rb
@@ -25,13 +25,16 @@ module Trimtool
 
           rails_new_command = 'docker-compose run app rails new . --database=postgresql --skip-bundle'
 
-          # Use the bootstrap application template
+          # Set the application template
           if options[:bootstrap] && options[:api]
-            copy_file 'bootstrap_template.rb', "#{snake_name}/bootstrap_template.rb"
-            rails_new_command.concat(' -m bootstrap_template.rb')
+            copy_file 'templates/api_bootstrap_template.rb', "#{snake_name}/api_bootstrap_template.rb"
+            rails_new_command.concat(' -m api_bootstrap_template.rb')
           elsif options[:bootstrap]
             puts '--bootstrap must be used with --api... Bootstrap base Rails apps soon.'
             return
+          else
+            copy_file 'templates/default_template.rb', "#{snake_name}/default_template.rb"
+            rails_new_command.concat(' -m default_template.rb')
           end
 
           # Copy this over so that Docker can run Rails new
@@ -58,28 +61,6 @@ module Trimtool
 
             # Run the command to generate a new rails app
             run(rails_new_command)
-
-            # Set the name of the app in application.rb
-            gsub_file('config/application.rb', 'module App', "module #{appname}")
-
-            # Edit database.yml to work with docker
-            insert_into_file('config/database.yml', "\s\susername: postgres\n\s\spassword:\n\s\shost: postgres\n\s\sport: 5432\n", :after => "default: &default\n")
-            gsub_file('config/database.yml', 'app_development', "#{snake_name}_development")
-            gsub_file('config/database.yml', 'app_test', "#{snake_name}_test")
-            gsub_file('config/database.yml', 'app_production', "#{snake_name}_production")
-            gsub_file('config/database.yml', 'username: app', "username: #{snake_name}")
-            gsub_file('config/database.yml', 'APP_DATABASE_PASSWORD', "#{snake_name.upcase}_DATABASE_PASSWORD")
-
-            # Edit the Dockerfile and rebuild the app now that it has a Gemfile and Gemfile.lock
-            gsub_file('Dockerfile', 'RUN gem install rails', '#RUN gem install rails')
-            gsub_file('Dockerfile', '#COPY Gemfile Gemfile.lock ./', 'COPY Gemfile Gemfile.lock ./')
-            gsub_file('Dockerfile', '#RUN gem install bundler && bundle install --jobs 20 --retry 5', 'RUN gem install bundler && bundle install --jobs 20 --retry 5')
-            run('docker-compose run app bundle')
-            run('docker-compose build')
-
-            # Setup the database (Create and Migrate)
-            run('docker-compose run app rake db:create')
-            run('docker-compose run app rake db:migrate')
 
           end
         end
---
+++ lib/trimtool/cli/rails/templates/api_bootstrap_template.rb
@@ -0,0 +1,69 @@
+# Install Gems
+# ----------------------------
+gem 'rails'
+gem 'pg'
+gem 'puma'
+gem 'bcrypt'
+gem 'rack-cors'
+gem 'active_model_serializers'
+gem 'jwt'
+gem 'knock'
+gem 'cancancan'
+gem 'rolify'
+gem 'seedbank'
+gem 'figaro'
+
+gem_group :development, :test do
+  gem 'byebug', platform: :mri
+  gem 'rspec-rails'
+  gem 'faker'
+  gem 'pry'
+  gem 'pry-byebug'
+  gem 'pry-clipboard'
+  gem 'pry-doc'
+  gem 'pry-docmore'
+  gem 'pry-rescue'
+  gem 'pry-stack_explorer'
+  gem 'pry-rails'
+  gem 'shoulda-matchers'
+end
+
+gem_group :development do
+  gem 'listen'
+  gem 'spring'
+  gem 'spring-watcher-listen'
+end
+
+gem_group :test do
+  gem 'simplecov', :require => false
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+#
+# App Setup
+# ----------------------------
+
+# Set the name of the app in application.rb
+gsub_file('config/application.rb', 'module App', "module #{appname}")
+
+# Edit database.yml to work with docker
+insert_into_file('config/database.yml', "\s\susername: postgres\n\s\spassword:\n\s\shost: postgres\n\s\sport: 5432\n", :after => "default: &default\n")
+gsub_file('config/database.yml', 'app_development', "#{snake_name}_development")
+gsub_file('config/database.yml', 'app_test', "#{snake_name}_test")
+gsub_file('config/database.yml', 'app_production', "#{snake_name}_production")
+gsub_file('config/database.yml', 'username: app', "username: #{snake_name}")
+gsub_file('config/database.yml', 'APP_DATABASE_PASSWORD', "#{snake_name.upcase}_DATABASE_PASSWORD")
+
+# Edit the Dockerfile and rebuild the app now that it has a Gemfile and Gemfile.lock
+gsub_file('Dockerfile', 'RUN gem install rails', '#RUN gem install rails')
+gsub_file('Dockerfile', '#COPY Gemfile Gemfile.lock ./', 'COPY Gemfile Gemfile.lock ./')
+gsub_file('Dockerfile', '#RUN gem install bundler && bundle install --jobs 20 --retry 5', 'RUN gem install bundler && bundle install --jobs 20 --retry 5')
+run('docker-compose run app bundle')
+run('docker-compose build')
+
+# Setup the database (Create and Migrate)
+run('docker-compose run app rake db:create')
+run('docker-compose run app rake db:migrate')
