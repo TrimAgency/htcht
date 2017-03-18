e5cdf7ec80dceb5f3d4a7e8c4d37082d47b9b887
Alex Miller <apmiller108@yahoo.com>
2017-03-17 23:11:16 -0400

    Add version and main specs to verify test suite runs.

--- spec/trimtool/cli/version_spec.rb
+++ spec/trimtool/cli/version_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe Trimtool::VERSION do
-
   it 'has a version number' do
-    expect(described_class).to_not be nil
+    expect(Trimtool::VERSION).to_not be nil
   end
 end
