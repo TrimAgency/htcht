ad5a49570e931fadbbcbc800bf3642d7bcbd8162
Stephen Taylor Witte <stevetwitte@gmail.com>
2018-03-02 14:16:55 -0500

    change name of command

--- lib/htcht/main.rb
+++ lib/htcht/main.rb
@@ -6,8 +6,8 @@ module Htcht
   # All subcommands go here
   class Main < Thor
 
-    desc "rails-api", "Creates a Ruby on Rails API with the Trim Starter Template"
-    subcommand "rails-api", Htcht::CLI::Rails::Rails
+    desc "rails", "Creates a Ruby on Rails API with the Trim Starter Template"
+    subcommand "rails", Htcht::CLI::Rails::Rails
 
   end
 
