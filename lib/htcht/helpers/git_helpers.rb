4a8dfd6b10f1bf3382e90bd4e219fd242f57dd4e
Stephen Witte <stevetwitte@gmail.com>
2018-04-13 10:45:20 -0400

    adds angular generator, git constant, dash case name hepler (#48)

--- lib/htcht/helpers/git_helpers.rb
+++ lib/htcht/helpers/git_helpers.rb
@@ -2,6 +2,7 @@ module Htcht
   module Helpers
     module GitHelpers
       RAILS_STARTER_URL = 'https://github.com/TrimAgency/rails-api-starter.git'.freeze
+      ANGULAR_STARTER_URL = 'https://github.com/TrimAgency/angular-2-starter-template.git'.freeze
     end
   end
 end
--- lib/htcht/main.rb
+++ lib/htcht/main.rb
@@ -1,14 +1,16 @@
 require 'thor'
 require 'htcht/cli/rails/rails'
+require 'htcht/cli/angular/angular'
 
 module Htcht
 
   # All subcommands go here
   class Main < Thor
+    desc 'rails', 'Creates a Ruby on Rails API with the Trim Starter Template'
+    subcommand 'rails', Htcht::CLI::Rails::Rails
 
-    desc "rails", "Creates a Ruby on Rails API with the Trim Starter Template"
-    subcommand "rails", Htcht::CLI::Rails::Rails
-
+    desc 'angular', 'Creates a Angular App with the Trim Starter Template'
+    subcommand 'angular', Htcht::CLI::Angular::Angular
   end
 
 end
